b09dd9833b3b970e14d0e32060a7c67495dbc95c
Andrushchenko, Oleksandr <o.andrushchenko@globallogic.com>
2014-11-27 14:01:06 +0200

    Add JNI

--- server/src/com/a2k/vncserver/MainActivity.java
+++ server/src/com/a2k/vncserver/MainActivity.java
@@ -2,8 +2,11 @@ package com.a2k.vncserver;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.a2k.vncserver.VncServerProto;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +14,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Log.d(getPackageCodePath(), m_VncProto.getVersion());
 	}
+
+	private static VncServerProto m_VncProto = new VncServerProto();
 }
---
+++ server/src/com/a2k/vncserver/VncServerProto.java
@@ -0,0 +1,9 @@
+package com.a2k.vncserver;
+
+public class VncServerProto {
+	public native String getVersion();
+
+	static {
+		System.loadLibrary("vncserver");
+	}
+}
