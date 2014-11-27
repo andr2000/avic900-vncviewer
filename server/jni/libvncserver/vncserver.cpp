c7c1a56318c94ee3e4e72244bf1ca5b943479c82
Oleksandr Andrushchenko <andr2000@gmail.com>
2014-11-27 14:01:06 +0200

    Add JNI

--- server/jni/libvncserver/vncserver.cpp
+++ server/jni/libvncserver/vncserver.cpp
@@ -1,1 +1,12 @@
 #include <jni.h>
+#include <string.h>
+
+#include "rfb/rfbconfig.h"
+
+extern "C"
+{
+	JNIEXPORT jstring JNICALL Java_com_a2k_vncserver_VncServerProto_getVersion(JNIEnv *env, jobject obj)
+	{
+		return env->NewStringUTF("libvncserver " LIBVNCSERVER_VERSION);
+	}
+}
