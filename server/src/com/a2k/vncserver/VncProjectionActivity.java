3f9c6eab6f1f8584b2824f1eca7f4a443280cf34
Andrushchenko, Oleksandr <o.andrushchenko@globallogic.com>
2014-11-28 16:17:34 +0200

    Add VNC projection activity base

---
+++ server/src/com/a2k/vncserver/VncProjectionActivity.java
@@ -0,0 +1,15 @@
+package com.a2k.vncserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VncProjectionActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vnc_projection);
+	}
+}
