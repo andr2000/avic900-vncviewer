885fe68c654d07432d13175f0524fb4abd6b9d79
Oleksandr Andrushchenko <andr2000@gmail.com>
2014-12-02 16:33:18 +0200

    Pass bitmap to JNI

--- server/src/com/a2k/vncserver/VncServerProto.java
+++ server/src/com/a2k/vncserver/VncServerProto.java
@@ -1,8 +1,11 @@
 package com.a2k.vncserver;
 
+import android.graphics.Bitmap;
+
 public class VncServerProto
 {
 	public native String getVersion();
+	public native void updateScreen(Bitmap bitmap);
 
 	static
 	{
