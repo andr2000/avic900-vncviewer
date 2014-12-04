b19d562c1c8da18ae0c5effbe2ea647d13f218d2
Andrushchenko, Oleksandr <o.andrushchenko@globallogic.com>
2014-12-04 10:33:19 +0200

    Draw video frames

--- server/src/com/a2k/vncserver/Gles.java
+++ server/src/com/a2k/vncserver/Gles.java
@@ -1,5 +1,10 @@
 package com.a2k.vncserver;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
@@ -16,11 +21,63 @@ public class Gles
 {
 	public static final String TAG = "Gles";
 
+	private static final String vertexShaderCode =
+		"attribute vec4 vPosition;" +
+		"attribute vec4 vTexCoordinate;" +
+		"uniform mat4 textureTransform;" +
+		"varying vec2 v_TexCoordinate;" +
+		"void main() {" +
+		"   v_TexCoordinate = (textureTransform * vTexCoordinate).xy;" +
+		"   gl_Position = vPosition;" +
+		"}";
+
+	private static final String fragmentShaderCode =
+		"#extension GL_OES_EGL_image_external : require\n" +
+		"precision mediump float;" +
+		"uniform samplerExternalOES texture;" +
+		"varying vec2 v_TexCoordinate;" +
+		"void main () {" +
+		"    vec4 color = texture2D(texture, v_TexCoordinate);" +
+		"    gl_FragColor = color;" +
+		"}";
+
+	private static float squareSize = 1.0f;
+	private static float squareCoords[] =
+	{
+		-squareSize,  squareSize, 0.0f,	// top left
+		-squareSize, -squareSize, 0.0f,	// bottom left
+		squareSize, -squareSize, 0.0f,	// bottom right
+		squareSize,  squareSize, 0.0f	// top right
+	};
+
+	private static short drawOrder[] = { 0, 1, 2, 0, 2, 3};
+
+	private FloatBuffer textureBuffer;
+	private float textureCoords[] =
+	{
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 0.0f, 1.0f
+	};
+
+	private int m_TextureWidth;
+	private int m_TextureHeight;
+	private int m_ViewWidth;
+	private int m_ViewHeight;
+
 	private EGL10 m_Egl;
 	private EGLDisplay m_EglDisplay;
 	private EGLContext m_EglContext;
 	private EGLSurface m_EglSurface;
 	private int m_EglTextures[] = new int[1];
+
+	private int m_VertexShaderHandle;
+	private int m_FragmentShaderHandle;
+	private int m_ShaderProgram;
+	private FloatBuffer m_VertexBuffer;
+	private ShortBuffer m_DrawListBuffer;
+	private boolean m_AdjustViewport = false;
 
 	private EGLConfig chooseEglConfig()
 	{
@@ -94,11 +151,21 @@ public class Gles
 
 	public void setupTexture(int width, int height)
 	{
+		m_TextureWidth = width;
+		m_TextureHeight = height;
+
+		ByteBuffer texturebb = ByteBuffer.allocateDirect(textureCoords.length * 4);
+		texturebb.order(ByteOrder.nativeOrder());
+
+		textureBuffer = texturebb.asFloatBuffer();
+		textureBuffer.put(textureCoords);
+		textureBuffer.position(0);
+
 		/* Generate the actual texture */
 		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 		GLES20.glGenTextures(1, m_EglTextures, 0);
 		checkGlError("Texture generate");
-		
+
 		GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, m_EglTextures[0]);
 		checkGlError("Texture bind");
 	}
@@ -111,6 +178,113 @@ public class Gles
 	public void swapBufers()
 	{
 		m_Egl.eglSwapBuffers(m_EglDisplay, m_EglSurface);
+		checkGlError("Swap buffers");
 	}
 
+	public void loadShaders()
+	{
+		m_VertexShaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+		GLES20.glShaderSource(m_VertexShaderHandle, vertexShaderCode);
+		GLES20.glCompileShader(m_VertexShaderHandle);
+		checkGlError("Vertex shader compile");
+
+		m_FragmentShaderHandle = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+		GLES20.glShaderSource(m_FragmentShaderHandle, fragmentShaderCode);
+		GLES20.glCompileShader(m_FragmentShaderHandle);
+		checkGlError("Pixel shader compile");
+
+		m_ShaderProgram = GLES20.glCreateProgram();
+		GLES20.glAttachShader(m_ShaderProgram, m_VertexShaderHandle);
+		GLES20.glAttachShader(m_ShaderProgram, m_FragmentShaderHandle);
+		GLES20.glLinkProgram(m_ShaderProgram);
+		checkGlError("Shader program compile");
+
+		int[] status = new int[1];
+		GLES20.glGetProgramiv(m_ShaderProgram, GLES20.GL_LINK_STATUS, status, 0);
+		if (status[0] != GLES20.GL_TRUE)
+		{
+			String error = GLES20.glGetProgramInfoLog(m_ShaderProgram);
+			Log.e("SurfaceTest", "Error while linking program:\n" + error);
+		}
+	}
+
+	public void setupVertexBuffer()
+	{
+		ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder. length * 2);
+		dlb.order(ByteOrder.nativeOrder());
+		m_DrawListBuffer = dlb.asShortBuffer();
+		m_DrawListBuffer.put(drawOrder);
+		m_DrawListBuffer.position(0);
+
+		/* Initialize the texture holder */
+		ByteBuffer bb = ByteBuffer.allocateDirect(squareCoords.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+
+		m_VertexBuffer = bb.asFloatBuffer();
+		m_VertexBuffer.put(squareCoords);
+		m_VertexBuffer.position(0);
+	}
+
+	private void adjustViewport()
+	{
+		float surfaceAspect = m_TextureHeight / (float)m_TextureWidth;
+		float viewAspect = m_ViewHeight / (float)m_ViewWidth;
+
+		if (surfaceAspect > viewAspect)
+		{
+			float heightRatio = m_TextureHeight / (float)m_ViewHeight;
+			int newWidth = (int)(m_TextureWidth * heightRatio);
+			int xOffset = (newWidth - m_TextureWidth) / 2;
+			GLES20.glViewport(-xOffset, 0, newWidth, m_TextureHeight);
+		}
+		else
+		{
+			float widthRatio = m_TextureWidth / (float)m_ViewWidth;
+			int newHeight = (int)(m_TextureHeight * widthRatio);
+			int yOffset = (newHeight - m_TextureHeight) / 2;
+			GLES20.glViewport(0, -yOffset, m_TextureWidth, newHeight);
+		}
+		m_AdjustViewport = false;
+	}
+
+	public void setViewport(int width, int height)
+	{
+		m_ViewHeight = height;
+		m_ViewWidth = width;
+		m_AdjustViewport = true;
+	}
+
+	public void draw(float videoTextureTransform[])
+	{
+		if (m_AdjustViewport)
+		{
+			adjustViewport();
+		}
+
+		GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+		/* Draw texture */
+		GLES20.glUseProgram(m_ShaderProgram);
+		int textureParamHandle = GLES20.glGetUniformLocation(m_ShaderProgram, "texture");
+		int textureCoordinateHandle = GLES20.glGetAttribLocation(m_ShaderProgram, "vTexCoordinate");
+		int positionHandle = GLES20.glGetAttribLocation(m_ShaderProgram, "vPosition");
+		int textureTranformHandle = GLES20.glGetUniformLocation(m_ShaderProgram, "textureTransform");
+
+		GLES20.glEnableVertexAttribArray(positionHandle);
+		GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 4 * 3, m_VertexBuffer);
+
+		GLES20.glBindTexture(GLES20.GL_TEXTURE0, m_EglTextures[0]);
+		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+		GLES20.glUniform1i(textureParamHandle, 0);
+
+		GLES20.glEnableVertexAttribArray(textureCoordinateHandle);
+		GLES20.glVertexAttribPointer(textureCoordinateHandle, 4, GLES20.GL_FLOAT, false, 0, textureBuffer);
+
+		GLES20.glUniformMatrix4fv(textureTranformHandle, 1, false, videoTextureTransform, 0);
+
+		GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, m_DrawListBuffer);
+		GLES20.glDisableVertexAttribArray(positionHandle);
+		GLES20.glDisableVertexAttribArray(textureCoordinateHandle);
+	}
 }
