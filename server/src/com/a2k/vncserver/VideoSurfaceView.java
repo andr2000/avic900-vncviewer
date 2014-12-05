d65c4ca1be857fceca27a10f0b21d536f527eb25
Andrushchenko, Oleksandr <o.andrushchenko@globallogic.com>
2014-12-05 16:24:21 +0200

    Remove video playback code

--- server/src/com/a2k/vncserver/VideoSurfaceView.java
+++ server/src/com/a2k/vncserver/VideoSurfaceView.java
@@ -1,15 +1,11 @@
 package com.a2k.vncserver;
 
-import java.io.IOException;
-
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
 import android.graphics.SurfaceTexture;
-import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
 import android.util.Log;
 import android.view.Surface;
 
@@ -17,21 +13,18 @@ import com.a2k.vncserver.VncJni;
 
 class VideoSurfaceView extends GLSurfaceView {
     private static final String TAG = "VideoSurfaceView";
-    private static final int SLEEP_TIME_MS = 1000;
 
     VideoRender mRenderer;
-    private MediaPlayer mMediaPlayer = null;
 
 	private long m_GraphicBuffer;
 	private VncJni m_VncJni = null;
 
-    public VideoSurfaceView(Context context, MediaPlayer mp, VncJni vncJni) {
+    public VideoSurfaceView(Context context, VncJni vncJni) {
         super(context);
 
         setEGLContextClientVersion(2);
-        mMediaPlayer = mp;
         m_VncJni = vncJni;
-        mRenderer = new VideoRender(context);
+        mRenderer = new VideoRender(context, this);
         super.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
         setRenderer(mRenderer);
     }
@@ -41,27 +34,26 @@ class VideoSurfaceView extends GLSurfaceView {
         queueEvent(new Runnable(){
                 public void run() {
                 	mRenderer.setVncJni(m_VncJni);
-                    mRenderer.setMediaPlayer(mMediaPlayer);
                 }});
 
         super.onResume();
     }
 
-    public void startTest() throws Exception {
-        Thread.sleep(SLEEP_TIME_MS);
-        mMediaPlayer.start();
-
-        Thread.sleep(SLEEP_TIME_MS * 5);
-        mMediaPlayer.setSurface(null);
-
-        while (mMediaPlayer.isPlaying()) {
-            Thread.sleep(SLEEP_TIME_MS);
-        }
+    private OnSurfaceCreatedListener onSurfaceCreatedListener;
+    public interface OnSurfaceCreatedListener
+    {
+        void onSurfaceCreated(Surface surface);
+    }
+    
+    public void setOnSurfaceCreatedListener(OnSurfaceCreatedListener listener) {
+    	onSurfaceCreatedListener = listener;
     }
 
-    public Surface getSurface()
-    {
-    	return mRenderer.getSurface();
+    public void onSurfaceCreated(Surface surface) {
+    	if (onSurfaceCreatedListener != null)
+    	{
+    		onSurfaceCreatedListener.onSurfaceCreated(surface);
+    	}
     }
 
     public void setSurfaceSize(int width, int height)
@@ -80,24 +72,18 @@ class VideoSurfaceView extends GLSurfaceView {
         private Surface mSurface;
         private SurfaceTexture mSurfaceTexture;
         private boolean updateSurface = false;
+        
+        private VideoSurfaceView m_Parent;
 
-        private MediaPlayer mMediaPlayer;
     	private VncJni m_VncJni;
 
-        public VideoRender(Context context) {
+        public VideoRender(Context context, VideoSurfaceView parent) {
+        	m_Parent = parent;
             mTextureRender = new TextureRender();
-        }
-
-        public void setMediaPlayer(MediaPlayer player) {
-            mMediaPlayer = player;
         }
 
         public void setVncJni(VncJni vncJni) {
             m_VncJni = vncJni;
-        }
-
-        public Surface getSurface() {
-            return mSurface;
         }
 
         public void setSurfaceSize(int width, int height)
@@ -130,18 +116,8 @@ class VideoSurfaceView extends GLSurfaceView {
              */
             mSurfaceTexture = new SurfaceTexture(mTextureRender.getTextureId());
             mSurfaceTexture.setOnFrameAvailableListener(this);
-
             mSurface = new Surface(mSurfaceTexture);
-            //mMediaPlayer.setSurface(mSurface);
-            //mSurface.release();
-
-            /*
-            try {
-                mMediaPlayer.prepare();
-            } catch (IOException t) {
-                Log.e(TAG, "media player prepare failed");
-            }
-            */
+            m_Parent.onSurfaceCreated(mSurface);
 
             synchronized(this) {
                 updateSurface = false;
