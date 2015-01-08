f7f5fdbd953323b7710fcbed954449c4ba692d5d
Andrushchenko, Oleksandr <o.andrushchenko@globallogic.com>
2015-01-08 15:33:59 +0200

    Codestyle cleanup

--- client/platform/wince/client_ddraw.cpp
+++ client/platform/wince/client_ddraw.cpp
@@ -6,11 +6,12 @@ rfbBool Client_DDraw::OnMallocFrameBuffer(rfbClient *client)
 	{
 		return FALSE;
 	}
-    hdcImage = CreateCompatibleDC(NULL);
-    if (!hdcImage) {
-	    return -1;
-    }
-    SelectObject(hdcImage, m_hBmp);
+	hdcImage = CreateCompatibleDC(NULL);
+	if (!hdcImage)
+	{
+		return -1;
+	}
+	SelectObject(hdcImage, m_hBmp);
 	return TRUE;
 }
 
@@ -18,28 +19,16 @@ void Client_DDraw::OnFrameBufferUpdate(rfbClient* client, int x, int y, int w, i
 	DEBUGMSG(TRUE, (_T("OnFrameBufferUpdate: x=%d y=%d w=%d h=%d\r\n"), x, y, w, h));
 
 	HRESULT ddrval;
-
-#if 0
-    DDSURFACEDESC2 ddsd;
-	ddsd.dwSize = sizeof(ddsd);
-    ddsd.dwFlags = DDSD_HEIGHT | DDSD_WIDTH;
-    lpBackBuffer->GetSurfaceDesc(&ddsd);
-#endif
-    HRESULT hr;
-    HDC hdc;
-    if ((hr = lpBackBuffer->GetDC(&hdc)) == DD_OK)
-    {
-        if (!BitBlt(hdc, 
-			x, y, 
-			w, 
-			h, 
-			hdcImage, 
-			x, y,
-			SRCCOPY))
-			hr = E_FAIL;
-        lpBackBuffer->ReleaseDC(hdc);
-    }
-    ddrval = lpFrontBuffer->Flip(NULL, 0);
+	HDC hdc;
+	if ((ddrval = lpBackBuffer->GetDC(&hdc)) == DD_OK)
+	{
+		if (!BitBlt(hdc, x, y, w, h, hdcImage, x, y, SRCCOPY))
+		{
+			ddrval = E_FAIL;
+		}
+		lpBackBuffer->ReleaseDC(hdc);
+	}
+	ddrval = lpFrontBuffer->Flip(NULL, 0);
 }
 
 void Client_DDraw::OnShutdown()
@@ -50,74 +39,55 @@ void Client_DDraw::OnShutdown()
 
 int Client_DDraw::Initialize(void *_private)
 {
-	DDCAPS ddcaps;
-	HRESULT ddrval;
-	DDSURFACEDESC2 ddsd;
-	DDSCAPS2 ddscaps;
-	DDSURFACEDESC2 DDSurfDesc;
-	LPDIRECTDRAW pDD;
-
 	lpDD = NULL;
 	lpFrontBuffer = NULL;
 	lpBackBuffer = NULL;
 
+	HRESULT ddrval;
+	LPDIRECTDRAW pDD;
 	ddrval = DirectDrawCreate(NULL, &pDD, NULL);
-
-    if (ddrval != DD_OK)
+	if (ddrval != DD_OK)
 	{
-        return -1;
+		return -1;
 	}
-    /* Fetch DirectDraw4 interface */
-    ddrval = pDD->QueryInterface(IID_IDirectDraw4, (LPVOID*)&lpDD);
-    if (ddrval != DD_OK)
+	/* Fetch DirectDraw4 interface */
+	ddrval = pDD->QueryInterface(IID_IDirectDraw4, (LPVOID*)&lpDD);
+	if (ddrval != DD_OK)
 	{
-        return -1;
+		return -1;
 	}
-    pDD->Release();
-    pDD = NULL;
+	pDD->Release();
+	pDD = NULL;
 
-    ddrval = lpDD->SetCooperativeLevel(m_hWnd,
-                            DDSCL_EXCLUSIVE | DDSCL_FULLSCREEN);
-    if (ddrval != DD_OK)
+	ddrval = lpDD->SetCooperativeLevel(m_hWnd, DDSCL_EXCLUSIVE | DDSCL_FULLSCREEN);
+	if (ddrval != DD_OK)
 	{
-        return -1;
+		return -1;
 	}
-
-    /* Get mode info */
-    memset(&DDSurfDesc, 0, sizeof(DDSurfDesc));
-    DDSurfDesc.dwSize = sizeof(DDSurfDesc);
-
-    ddrval = lpDD->GetDisplayMode( &DDSurfDesc );
-    if ( ddrval != DD_OK )
+	/* Create surface */
+	DDSURFACEDESC2 ddsd;
+	memset(&ddsd, 0, sizeof(ddsd));
+	ddsd.dwSize = sizeof( ddsd );
+	ddsd.dwFlags = DDSD_CAPS | DDSD_BACKBUFFERCOUNT;
+	ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE |
+		DDSCAPS_FLIP | DDSCAPS_COMPLEX;
+	ddsd.dwBackBufferCount = 1;
+	ddrval = lpDD->CreateSurface(&ddsd, &lpFrontBuffer, NULL);
+	if (ddrval != DD_OK)
 	{
-        return -1;
+		if (ddrval == DDERR_NOFLIPHW)
+		{
+			return -1;
+		}
+		return -1;
 	}
-    ddcaps.dwSize = sizeof( ddcaps );
-
-    // Create surface
-    memset( &ddsd, 0, sizeof( ddsd ) );
-    ddsd.dwSize = sizeof( ddsd );
-    ddsd.dwFlags = DDSD_CAPS | DDSD_BACKBUFFERCOUNT;
-    ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE |
-                          DDSCAPS_FLIP |
-                          DDSCAPS_COMPLEX;
-    ddsd.dwBackBufferCount = 1;
-    ddrval = lpDD->CreateSurface( &ddsd, &lpFrontBuffer, NULL );
-    if (ddrval != DD_OK)
-    {
-        if (ddrval == DDERR_NOFLIPHW)
-            return -1;
-  
-        return -1;
-    }
-
-    // get a pointer to the back buffer
-    ddscaps.dwCaps = DDSCAPS_BACKBUFFER;
-    ddrval = lpFrontBuffer->GetAttachedSurface(
-                &ddscaps,
-                &lpBackBuffer );
-    if( ddrval != DD_OK )
-        return -1;
-
+	/* get a pointer to the back buffer */
+	DDSCAPS2 ddscaps;
+	ddscaps.dwCaps = DDSCAPS_BACKBUFFER;
+	ddrval = lpFrontBuffer->GetAttachedSurface(&ddscaps, &lpBackBuffer);
+	if (ddrval != DD_OK)
+	{
+		return -1;
+	}
 	return Client_WinCE::Initialize(_private);
 }
